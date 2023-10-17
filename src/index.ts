e184ec6903589049b3fa4eda5ed8f25148fb9494
sajjany47 <sajjany47@gmail.com>
2023-10-17 23:44:43 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("sajjan");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+const port = 8081;
+
+mongoose
+  .connect("mongodb://localhost:27017/employee")
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch((e: any) => console.log(e));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
