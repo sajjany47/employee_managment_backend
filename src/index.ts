f2c91a6bf688ea9d09d14c4de8cb9730c248a6a5
QbAnurag <anurag.patra@qbent.in>
2023-10-19 18:46:36 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 const port = 8081;
 
-mongoose
-  .connect("mongodb://localhost:27017/employee")
-  .then(() => {
-    console.log("Database Connected Successfully");
-  })
-  .catch((e: any) => console.log(e));
+function main() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
+  app.use(cors());
+  mongoose
+    .connect(
+      "mongodb+srv://sajjany47:s%40JJAN888@cluster0.g6om3i4.mongodb.net/employee?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log("Database Connected Successfully");
+      app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+    })
+    .catch((e: any) => console.log(e));
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+main();
