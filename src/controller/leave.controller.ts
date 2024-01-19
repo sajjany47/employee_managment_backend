0efb4c389509743dc379d9d04240f628658b4c57
sajjany47 <sajjany47@gmail.com>
2024-01-19 19:01:07 +0530

    leave apply

--- src/controller/leave.controller.ts
+++ src/controller/leave.controller.ts
@@ -4,6 +4,7 @@ import leave from "../model/leave.model";
 import moment from "moment";
 import user from "../model/user.model";
 import mongoose from "mongoose";
+import holidayList from "../model/holiday.model";
 
 const leaveAlloted = async (req: Request, res: Response) => {
   try {
@@ -54,14 +55,14 @@ const leaveAlloted = async (req: Request, res: Response) => {
       });
 
       const saveLeaveList = await createLeaveList.save();
-      if (saveLeaveList) {
-        const isLeaveAllocatedUpdate = await user.findOneAndUpdate(
-          {
-            username: reqData.user_id,
-          },
-          { $set: { isLeaveAllocated: true } }
-        );
-      }
+      // if (saveLeaveList) {
+      //   const isLeaveAllocatedUpdate = await user.findOneAndUpdate(
+      //     {
+      //       username: reqData.user_id,
+      //     },
+      //     { $set: { isLeaveAllocated: true } }
+      //   );
+      // }
 
       res
         .status(StatusCodes.OK)
@@ -164,6 +165,123 @@ const editLeaveAlloctated = async (req: Request, res: Response) => {
     );
 
     res.status(StatusCodes.OK).json({ message: "Leave updated successfully" });
+  } catch (error: any) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+  }
+};
+
+const leaveApply = async (req: Request, res: Response) => {
+  try {
+    const reqData = Object.assign({}, req.body);
+    const startDay = reqData.startDay;
+    const endDay = reqData.endDay;
+    let weekends = [];
+
+    for (
+      let date = startDay.clone();
+      date.isSameOrBefore(endDay);
+      date.add(1, "days")
+    ) {
+      if (date.weekday() !== 6 || date.weekday() !== 0) {
+        weekends.push(date);
+      }
+    }
+
+    if (weekends.length > 0) {
+      // const findHoliday = await holidayList.findOne({
+      //   holidayYear: moment(reqData.startDay).format("YYYY"),
+      // });
+      // const a: any =
+      //   findHoliday === undefined || findHoliday === null
+      //     ? []
+      //     : findHoliday.holidayList;
+
+      // const filterDate = weekends.filter(
+      //   (item1) =>
+      //     !a.some(
+      //       (item2: any) =>
+      //         moment(item1).format("YYYY") ===
+      //         moment(item2.holidayDate).format("YYYY")
+      //     )
+      // );
+
+      const findUser = await leave.aggregate([
+        {
+          $unwind: {
+            path: "$leaveDetail",
+          },
+        },
+        {
+          $match: {
+            $and: [
+              { user_id: reqData.user_id },
+              {
+                "leaveDetail.leaveYear": moment(reqData.startDay).format(
+                  "YYYY"
+                ),
+              },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: "holidaylists",
+            localField: "leaveDetail.leaveYear",
+            foreignField: "holidayYear",
+            as: "result",
+          },
+        },
+
+        {
+          $unwind: {
+            path: "$result",
+          },
+        },
+      ]);
+
+      const filterDate: any = weekends.filter(
+        (item1: any) =>
+          !findUser[0]?.result?.holidayList.some(
+            (item2: any) =>
+              moment(item1).format("YYYY") ===
+              moment(item2.holidayDate).format("YYYY")
+          )
+      );
+
+      const totalLeaveUse = findUser[0].leaveDetail.leaveUseDetail.filter(
+        (item: any) => item.leaveStatus === "approved"
+      );
+
+      const updateLeave = await leave.findOneAndUpdate(
+        {
+          user_id: reqData.user_id,
+          "leaveDetail.leaveYear": moment(reqData.startDay).format("YYYY"),
+        },
+        {
+          $set: {
+            "leaveDetail.$.totalLeaveLeft":
+              findUser[0].leaveDetail.totalLeave - totalLeaveUse.length,
+            $push: {
+              "leaveDetail.$.leaveUseDetail": {
+                startDay: new Date(reqData.startDay),
+                endDay: new Date(reqData.endDay),
+                totalDays: filterDate.length,
+                leaveStatus: "pending",
+                approvedBy: null,
+              },
+            },
+          },
+        }
+      );
+
+      res
+        .status(StatusCodes.OK)
+        .json({ message: "Leave successfully apply, wait for approved" });
+    } else {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Select valid date for leave" });
+    }
   } catch (error: any) {
     res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
   }
