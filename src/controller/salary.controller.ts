27b95e4691b11ad567f1a567eaeb2ba9b6323ffd
sajjany47 <sajjany47@gmail.com>
2024-03-03 12:50:41 +0530

    changes

--- src/controller/salary.controller.ts
+++ src/controller/salary.controller.ts
@@ -2,57 +2,105 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import salary from "../model/salary.model";
 import user from "../model/user.model";
+import mongoose from "mongoose";
 
 const userSalaryCreate = async (req: Request, res: Response) => {
   try {
     const reqData = Object.assign({}, req.body);
     const findUser = await salary.findOne({ username: reqData.username });
     if (findUser) {
-      const userSalaryUpdate = await salary.findOneAndUpdate(
-        { username: reqData.username },
-        {
-          $set: {
-            currentSalary: {
-              basicSalary: reqData.basicSalary,
-              hra: reqData.hra,
-              travelAllowance: reqData.travelAllowance,
-              MedicalAllowance: reqData.MedicalAllowance,
-              LeaveTravelAllowance: reqData.LeaveTravelAllowance,
-              SpecialAllowance: reqData.SpecialAllowance,
-              providentFund: reqData.providentFund,
-              professionalTax: reqData.professionalTax,
-              incomeTax: reqData.incomeTax,
-              incrementType: reqData.incrementType,
-              incrementValue: reqData.incrementValue,
-              totalEarning: reqData.totalEarning,
-              updatedBy: reqData.updatedBy,
-              date: reqData.date,
-            },
+      if (reqData.type === "changes") {
+        const userSalaryUpdate = await salary.findOneAndUpdate(
+          {
+            username: reqData.username,
+            "salaryHistory._id": new mongoose.Types.ObjectId(reqData.id),
           },
-          $push: {
-            salaryHistory: {
-              basicSalary: reqData.basicSalary,
-              hra: reqData.hra,
-              travelAllowance: reqData.travelAllowance,
-              MedicalAllowance: reqData.MedicalAllowance,
-              LeaveTravelAllowance: reqData.LeaveTravelAllowance,
-              SpecialAllowance: reqData.SpecialAllowance,
-              providentFund: reqData.providentFund,
-              professionalTax: reqData.professionalTax,
-              incomeTax: reqData.incomeTax,
-              incrementType: reqData.incrementType,
-              incrementValue: reqData.incrementValue,
-              totalEarning: reqData.totalEarning,
-              updatedBy: reqData.updatedBy,
-              date: reqData.date,
+          {
+            $set: {
+              currentSalary: {
+                basicSalary: reqData.basicSalary,
+                hra: reqData.hra,
+                travelAllowance: reqData.travelAllowance,
+                MedicalAllowance: reqData.MedicalAllowance,
+                LeaveTravelAllowance: reqData.LeaveTravelAllowance,
+                SpecialAllowance: reqData.SpecialAllowance,
+                providentFund: reqData.providentFund,
+                professionalTax: reqData.professionalTax,
+                incomeTax: reqData.incomeTax,
+                incrementType: reqData.incrementType,
+                incrementValue: reqData.incrementValue,
+                totalEarning: reqData.totalEarning,
+                updatedBy: reqData.updatedBy,
+                date: reqData.date,
+              },
+
+              "salaryHistory.$.hra": reqData.hra,
+              "salaryHistory.$.basicSalary": reqData.basicSalary,
+              "salaryHistory.$.travelAllowance": reqData.travelAllowance,
+              "salaryHistory.$.MedicalAllowance": reqData.MedicalAllowance,
+              "salaryHistory.$.LeaveTravelAllowance":
+                reqData.LeaveTravelAllowance,
+              "salaryHistory.$.SpecialAllowance": reqData.SpecialAllowance,
+              "salaryHistory.$.providentFund": reqData.providentFund,
+              "salaryHistory.$.professionalTax": reqData.professionalTax,
+              "salaryHistory.$.incomeTax": reqData.incomeTax,
+              "salaryHistory.$.totalEarning": reqData.totalEarning,
+              "salaryHistory.$.updatedBy": reqData.updatedBy,
+              "salaryHistory.$.date": reqData.date,
             },
-          },
-        }
-      );
-      return res.status(StatusCodes.OK).json({
-        message: "User salary structure updated successfully",
-        data: userSalaryUpdate,
-      });
+          }
+        );
+        return res.status(StatusCodes.OK).json({
+          message: "User salary structure updated successfully",
+          data: userSalaryUpdate,
+        });
+      } else {
+        const userSalaryUpdate = await salary.findOneAndUpdate(
+          { username: reqData.username },
+          {
+            $set: {
+              currentSalary: {
+                basicSalary: reqData.basicSalary,
+                hra: reqData.hra,
+                travelAllowance: reqData.travelAllowance,
+                MedicalAllowance: reqData.MedicalAllowance,
+                LeaveTravelAllowance: reqData.LeaveTravelAllowance,
+                SpecialAllowance: reqData.SpecialAllowance,
+                providentFund: reqData.providentFund,
+                professionalTax: reqData.professionalTax,
+                incomeTax: reqData.incomeTax,
+                incrementType: reqData.incrementType,
+                incrementValue: reqData.incrementValue,
+                totalEarning: reqData.totalEarning,
+                updatedBy: reqData.updatedBy,
+                date: reqData.date,
+              },
+            },
+            $push: {
+              salaryHistory: {
+                basicSalary: reqData.basicSalary,
+                hra: reqData.hra,
+                travelAllowance: reqData.travelAllowance,
+                MedicalAllowance: reqData.MedicalAllowance,
+                LeaveTravelAllowance: reqData.LeaveTravelAllowance,
+                SpecialAllowance: reqData.SpecialAllowance,
+                providentFund: reqData.providentFund,
+                professionalTax: reqData.professionalTax,
+                incomeTax: reqData.incomeTax,
+                incrementType: reqData.incrementType,
+                incrementValue: reqData.incrementValue,
+                totalEarning: reqData.totalEarning,
+                updatedBy: reqData.updatedBy,
+                date: reqData.date,
+              },
+            },
+          }
+        );
+        return res.status(StatusCodes.OK).json({
+          message: "User salary structure updated successfully",
+          data: userSalaryUpdate,
+        });
+      }
     } else {
       const userSalary = new salary({
         currentSalary: {
@@ -65,8 +113,8 @@ const userSalaryCreate = async (req: Request, res: Response) => {
           providentFund: reqData.providentFund,
           professionalTax: reqData.professionalTax,
           incomeTax: reqData.incomeTax,
-          incrementType: reqData.incrementType,
-          incrementValue: reqData.incrementValue,
+          incrementType: null,
+          incrementValue: null,
           totalEarning: reqData.totalEarning,
           updatedBy: reqData.updatedBy,
           date: reqData.date,
