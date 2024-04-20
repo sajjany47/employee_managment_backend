c984f169bc5111c1ad9bf4bef3d6874bdcb1d9dd
sajjany47 <sajjany47@gmail.com>
2024-04-20 15:32:13 +0530

    socket chat

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,35 @@ import mongoose from "mongoose";
 import cors from "cors";
 import routes from "./routes/routes";
 import cookiesession from "cookie-session";
-import http from "http";
 import { Server } from "socket.io";
+import { createServer } from "http";
 
 function main() {
   const port = process.env.PORT || 8081;
   const mongodb_url: any = process.env.mongodb_url;
   const cookieSecretKey: any = process.env.COOKIE_SECRET;
-  const app = express();
-  const server = http.createServer(app);
-  const io = new Server(server);
+  const app: any = express();
+  const server: any = createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
 
   // const corsOptions = {
   //   origin: "http://localhost:8081",
   // };
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ limit: "30 mb", extended: true }));
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
   app.use("/employee/api", routes);
   app.use(
     cookiesession({
@@ -31,14 +43,15 @@ function main() {
   );
 
   io.on("connection", (socket) => {
-    console.log("a user connected");
+    console.log("a user connected", socket.id);
 
-    socket.on("chat message", (msg) => {
-      io.emit("chat message", msg);
+    socket.on("message", (msg) => {
+      console.log(msg);
+      io.emit("receive-message", msg);
     });
 
     socket.on("disconnect", () => {
-      console.log("user disconnected");
+      console.log("user disconnected", socket.id);
     });
   });
   mongoose
